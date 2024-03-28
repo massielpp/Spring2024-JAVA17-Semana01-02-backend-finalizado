eb9372e937076a4e2255c306a0ba2f78599c2ba5
massielpp <massielpisfil05@gmail.com>
2024-03-27 20:33:18 -0500

    tarea

---
+++ src/main/java/com/empresa/controller/MedicamentoController.java
@@ -0,0 +1,33 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.entity.Medicamento;
+import com.empresa.service.MedicamentoService;
+
+@RestController
+@RequestMapping("/url/medicamento")
+public class MedicamentoController {
+
+	@Autowired
+	private MedicamentoService medicamentoService;
+	
+	
+	@GetMapping
+	public List<Medicamento> listaAlumno(){
+		List<Medicamento> lstSalida = medicamentoService.listaMedicamento();
+		return lstSalida;
+	}
+	
+}
+
+
+
+
+
+
---
+++ src/main/java/com/empresa/service/MedicamentoService.java
@@ -0,0 +1,10 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Medicamento;
+
+public interface MedicamentoService {
+
+	public abstract List<Medicamento> listaMedicamento();
+}
